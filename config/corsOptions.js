a82da3965ee8f2d23355772ab139d44e3fe94293
Scott Wagner <s_wagner4@mail.fhsu.edu>
2024-05-08 21:45:24 -0500

    commit 21

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,5 @@
 const whitelist = [
-   'https://plucky-loud-crowd.glitch.me/',
+   'https://curious-judicious-art.glitch.me',
    'https://dazzling-snickerdoodle-777101.netlify.app', 
    'http://127.0.0.1:5500', 
    'http://localhost:3500'
