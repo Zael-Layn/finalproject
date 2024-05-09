a2c33e30af4035360e68fdd2813b9a5d32c4c772
Scott Wagner <s_wagner4@mail.fhsu.edu>
2024-05-08 21:46:42 -0500

    commit 25

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,17 @@
-const whitelist = [
-   'https://curious-judicious-art.glitch.me',
-   'https://dazzling-snickerdoodle-777101.netlify.app', 
-   'http://127.0.0.1:5500', 
-   'http://localhost:3500'
-];
-const corsOptions = {
-    origin: (origin, callback) =>{
-   if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-    } else {
-        callback(new Error('Not Allowed By CORS'));
-    }
-}, optionSuccessStatus: 20
-};
+// const whitelist = [
+//    'https://curious-judicious-art.glitch.me',
+//    'https://dazzling-snickerdoodle-777101.netlify.app', 
+//    'http://127.0.0.1:5500', 
+//    'http://localhost:3500'
+// ];
+// const corsOptions = {
+//     origin: (origin, callback) =>{
+//    if (whitelist.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//     } else {
+//         callback(new Error('Not Allowed By CORS'));
+//     }
+// }, optionSuccessStatus: 20
+// };
 
-module.exports = corsOptions;
+// module.exports = corsOptions;
