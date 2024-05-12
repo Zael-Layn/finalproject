c6f88f73c47031b8392361c8873c9a17e27669a7
Scott Wagner <143025638+Zael-Layn@users.noreply.github.com>
2024-05-12 15:30:27 -0700

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://swagner4:Eclipse7@cluster0.hvscj6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(uri, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
+        console.log('Connected to MongoDB');
     } catch (err) {
         console.error(err);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
